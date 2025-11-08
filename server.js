e866c9d8639ee8e52ea594e06703c2bed2b01975
noyan15 <hammadbaseer4@gmail.com>
2025-11-08 18:48:39 +0500

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const {sum} = require('./app')
 const Developer = require('./models/developerModel')
 const User = require('./models/userModel')
+const Blog = require('./models/blogModel')
 
 const app = express(); 
 
@@ -50,6 +51,91 @@ var developers = [
 
 
 // custom middlewares
+
+
+
+
+
+
+
+// auth module
+
+// registration api
+app.post('/register', async (request, response) => {
+    const {firstName, lastName, email, age, password, confirmPassword} = request.body;
+
+    if(password != confirmPassword){
+        return response.send('password and confirm password do not match')
+    }
+
+    const user = User.create({
+        firstName,
+        lastName,
+        email,
+        age,
+        password
+    })
+
+    response.send('user registered successfully')
+})
+
+app.post('/login', async (req, res)=>{
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email: email});
+
+    if(user === null || user === undefined){
+        return res.send('Account does not exist or email is incorrect')
+    }
+
+    if(user.password !== password){
+        res.send('Password is incorrect')
+    }else {
+        res.send({
+            message: 'Login successful',
+            user: user
+        })
+    }
+})
+
+// change password api
+app.post('/change-password', async (req,res)=>{
+    const {email, oldPassword, newPassword, confirmNewPassword} = req.body;
+
+    const user = await User.findOne({email: email});
+
+    if(user === null || user === undefined){
+        return res.send('Account does not exist or email is incorrect')
+    }
+
+    if(user.password !== oldPassword){
+        return res.send('Old password is incorrect')
+    }
+
+    if(newPassword !== confirmNewPassword){
+        return res.send('New password and confirm new password do not match')
+    }
+
+    user.password = newPassword;
+    await user.save();
+
+    res.send('Password changed successfully')
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // health api
